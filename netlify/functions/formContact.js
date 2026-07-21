398acd6b7008413f5c24174024d28b1ca9575379
Andres Zapata <medstrings6@gmail.com>
2026-07-21 00:23:37 -0500

    email function update

--- netlify/functions/formContact.js
+++ netlify/functions/formContact.js
@@ -1,4 +1,42 @@
 import process from "process";
+const { config } = require("dotenv");
+config();
+
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  },
+})
+
+const sendEmail = (mail) => {
+  return new Promise((resolve, reject) => {
+    transport.sendEmail(mail,
+      (error, _) => {
+        error ?
+          reject({
+            statusCode: 500,
+            body: error
+          })
+          : resolve({
+            statusCode: 200,
+            body: "Email Successfully sended to" + mail.to,
+          })
+      })
+  });
+}
+
+const generateBodyMsg = (params) => {
+  return `Formulario de contacto enviado por ${params.name} - ${params.email}.
+  con el tema: ${params.title} y el siguiente mensaje:
+    ${params.subject}
+  `;
+}
 
 export const handler = async (event, context) => {
   const headers = {
@@ -10,7 +48,14 @@ export const handler = async (event, context) => {
   switch (event.httpMethod) {
     case "POST":
       const data = JSON.parse(event.body);
+
       console.log("Recibí una sollicitud", data);
+      return await sendEmail({
+        from: process.env.EMAIL_USER,
+        to: "medstrings6@gmail.com",
+        subject: "Contacto - " + data.subject,
+        html: generateBodyMsg(data)
+      });
       //
       return;
     default:
